aef07306d9cf4d216587808c018db1456080234a
boreznik <38987034+boreznik@users.noreply.github.com>
2019-02-07 10:14:33 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+$("#click").click(function() {
+   $("#click").text("double click the header");
+});
+ 
+ $("#cclick").click(function() {
+   $("#cclick").image("");
+});
+ 
+ $("#head").dblclick(function() {
+   $("#clik").fadeOut() ;
+});
+ 
+ $("#clickme").click(function() {
+   $("#chest").fadeOut() ;
+});
+
+$("#imgg").hover(function() {
+   $("#chest").fadeIn() ;
+});
+
+$("#imgg").dblclick(function() {
+ 
+   $(".end").remove() ;
+});
+
+
+ 
